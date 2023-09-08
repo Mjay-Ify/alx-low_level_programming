4e757d46881e2e3f9100b33ae89ba8384c9014ab
Mjay-Ify <maryjaneasita@gmail.com>
2023-09-08 12:34:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: when successful
+ */
+
+int main(void)
+{
+	int u;
+
+	for (u = 0 ; u < 10 ; u++)
+		putchar(u + '0');
+	putchar('\n');
+	return (0);
+}
