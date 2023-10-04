e5e9d23afe9c545122452df889652751bb01a7a9
Mjay-Ify <maryjaneasita@gmail.com>
2023-10-04 16:45:08 +0100

    updated

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,35 +1,29 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * _atoi - a function that converts string to integer
- * @s: An input string
- * Return: integer from conversion
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer we get.
  */
 int _atoi(char *s)
 {
 	int sign = 1;
-	unsigned int total = 0;
-	char null_flag = 0;
+	unsigned int num = 0;
 
-	while (*s)
-	{
+	do {
 		if (*s == '-')
 			sign *= -1;
 
-		if (*s >= '0' && *s <= '9')
-		{
-			null_flag = 1;
-			total = total * 10 + *s - '0';
-		}
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-		else if (null_flag)
+		else if (num > 0)
 			break;
-		s++;
-	}
 
-	if (sign < 0)
-		total = (-total);
+	} while (*s++);
 
-	return (total);
+	return (num * sign);
 }
 
