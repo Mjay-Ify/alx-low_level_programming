2c2b743c8cf419aed66ef348867885948c1056b7
Mjay-Ify <maryjaneasita@gmail.com>
2023-10-04 15:53:59 +0100

    1-args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - A program that display the count of arguments provided as input.
  * @argc: The parameter count passed to the program.
  * @argv: The argument data provided
- * Return: always 0 
+ * Return: always 0
  */
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
